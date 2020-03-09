e438979b132ecb844064d92b2e0e12d7df3ec3bf
Artur Iatsko <arturyatsko@gmail.com>
2020-03-09 19:00:48 +0000

    Menu connecting

--- db/database.js
+++ db/database.js
@@ -2,49 +2,49 @@ const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString : connectionString
+  connectionString: connectionString
 });
 
 // get all the menu items
-const getAllItems = function() {
+const getAllItems = function () {
   return pool.query(`
   SELECT *
   FROM items
   WHERE active = true
   `)
-  .then(res => res.rows[0])
+    .then(res => res.rows[0])
 }
 exports.getAllItems = getAllItems;
 
 
-const getCompletedOrders = function() {
+const getCompletedOrders = function () {
   return pool.query(`
   SELECT *
   FROM orders
   WHERE status = 'completed'
   `)
-  .then(res => res.rows[0])
+    .then(res => res.rows[0])
 }
 exports.getCompletedOrders = getCompletedOrders;
 
 
-const getPendingOrders = function() {
+const getPendingOrders = function () {
   return pool.query(`
   SELECT *
   FROM orders
   WHERE status = 'pending'
   `)
-  .then(res => res.rows[0])
+    .then(res => res.rows[0])
 }
 exports.getPendingOrders = getPendingOrders;
 
 
-const getAcceptedOrders = function() {
+const getAcceptedOrders = function () {
   return pool.query(`
   SELECT *
   FROM orders
   WHERE status = 'accepted'
   `)
-  .then(res => res.rows[0])
+    .then(res => res.rows[0])
 }
 exports.getAcceptedOrders = getAcceptedOrders;
--- routes/users.js
+++ routes/users.js
@@ -7,21 +7,43 @@
 
 const express = require("express");
 const router = express.Router();
-const path = require("path");
+
+// Getting items from DB
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+  connectionString: connectionString
+});
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    //! example page
+    return pool.query(`
+    SELECT *
+    FROM items
+    WHERE active = true
+    `)
+      .then(response => {
+        let items = response.rows;
+        res.render("menu", items);
+        console.log(response.rows)
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
+
+
     res.render("menu");
 
-    db.query(`SELECT * FROM users;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res.status(500).json({ error: err.message });
-      });
+    /*     db.query(`SELECT * FROM users;`)
+          .then(data => {
+            const users = data.rows;
+            res.json({ users });
+          })
+          .catch(err => {
+            res.status(500).json({ error: err.message });
+          }); */
   });
 
   // show the menu with all items
