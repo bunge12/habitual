1e7b870f415a4cec65bea08666b37d4f3702dd8e
Shuchita <shuchita@live.com>
2020-03-09 19:37:32 +0000

    update image urls

--- db/seeds/01_items.sql
+++ db/seeds/01_items.sql
@@ -1,25 +1,25 @@
 -- items table seeds here
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Soft drink', 'Coca-cola', 149, 'https://www.pexels.com/photo/coca-cola-glass-bottle-macro-photography-2983100/');
+VALUES ('Soft drink', 'Coca-cola', 149, 'https://images.pexels.com/photos/2983100/pexels-photo-2983100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, price)
 VALUES ('Water Bottle', 199);
 INSERT INTO items (name, price)
 VALUES ('Orange Juice', 299);
 INSERT INTO items (name, price, picture_url)
-VALUES ('Iced Tea', 99, 'https://www.pexels.com/photo/lemon-iced-tea-with-lemon-fruits-792613/');
+VALUES ('Iced Tea', 99, 'https://images.pexels.com/photos/792613/pexels-photo-792613.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Corona', 'Everyone''s favourite virus - we mean, beer!', 299, 'https://www.pexels.com/photo/corona-extra-beer-bottle-2286972/');
+VALUES ('Corona', 'Everyone''s favourite virus - we mean, beer!', 299, 'https://images.pexels.com/photos/2286972/pexels-photo-2286972.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Mini Martini', 'Tequila, Triple Sec & fresh lime juice with a salted rim', 859, 'https://www.pexels.com/photo/close-up-photo-of-margarita-3407782/');
+VALUES ('Mini Martini', 'Tequila, Triple Sec & fresh lime juice with a salted rim', 859, 'https://images.pexels.com/photos/3407782/pexels-photo-3407782.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Caesar', 'Vodka, clamato juice and veggie garnishes', 659, 'https://www.pexels.com/photo/red-glass-with-garnished-beverage-2842876/');
+VALUES ('Caesar', 'Vodka, clamato juice and veggie garnishes', 659, 'https://images.pexels.com/photos/2842876/pexels-photo-2842876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Yum Yum Burger', 'Delicious hamburger served with fries', 199, 'https://www.pexels.com/photo/plate-of-fries-and-burger-3219483/');
+VALUES ('Yum Yum Burger', 'Delicious hamburger served with fries', 199, 'https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Fettuccini', 'Chicken breast, shrimp, tomatoes, beans and marinara sauce', 1099, 'https://www.pexels.com/photo/pasta-on-white-plate-on-focus-photo-1527603/');
+VALUES ('Fettuccini', 'Chicken breast, shrimp, tomatoes, beans and marinara sauce', 1099, 'https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url)
-VALUES ('Pepperoni Pizza', 'Topped with mushrooms, peppers, olives, tomatoes and pickles', 1099, 'https://www.pexels.com/photo/pepperoni-pizza-803290/');
+VALUES ('Pepperoni Pizza', 'Topped with mushrooms, peppers, olives, tomatoes and pickles', 1099, 'https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
 INSERT INTO items (name, description, price, picture_url, active)
-VALUES ('Lollipops', 'candy flavoured', 059, 'https://www.pexels.com/photo/pink-and-grey-heart-shape-lollipop-on-clear-glass-jar-40834/', false);
+VALUES ('Lollipops', 'candy flavoured', 059, 'https://images.pexels.com/photos/40834/eat-sugar-calories-food-40834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', false);
 INSERT INTO items (name, description, price, picture_url, active)
-VALUES ('Icecream', 'Icy and creamy!', 159, 'https://www.pexels.com/photo/dessert-ice-cream-summer-sweet-3631/', false);
+VALUES ('Icecream', 'Icy and creamy!', 159, 'https://images.pexels.com/photos/3631/summer-dessert-sweet-ice-cream.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', false);
