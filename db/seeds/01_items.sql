d637977fefbbbec364c6124b0447e3cf4f6a2212
Artur Iatsko <arturyatsko@gmail.com>
2020-03-09 19:48:13 +0000

    Seeds updated

--- db/seeds/01_items.sql
+++ db/seeds/01_items.sql
@@ -1,25 +1,51 @@
 -- items table seeds here
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Soft drink', 'Coca-cola', 149, 'https://images.pexels.com/photos/2983100/pexels-photo-2983100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, price)
-VALUES ('Water Bottle', 199);
-INSERT INTO items (name, price)
-VALUES ('Orange Juice', 299);
-INSERT INTO items (name, price, picture_url)
-VALUES ('Iced Tea', 99, 'https://images.pexels.com/photos/792613/pexels-photo-792613.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Corona', 'Everyone''s favourite virus - we mean, beer!', 299, 'https://images.pexels.com/photos/2286972/pexels-photo-2286972.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Mini Martini', 'Tequila, Triple Sec & fresh lime juice with a salted rim', 859, 'https://images.pexels.com/photos/3407782/pexels-photo-3407782.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Caesar', 'Vodka, clamato juice and veggie garnishes', 659, 'https://images.pexels.com/photos/2842876/pexels-photo-2842876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Yum Yum Burger', 'Delicious hamburger served with fries', 199, 'https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Fettuccini', 'Chicken breast, shrimp, tomatoes, beans and marinara sauce', 1099, 'https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url)
-VALUES ('Pepperoni Pizza', 'Topped with mushrooms, peppers, olives, tomatoes and pickles', 1099, 'https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
-INSERT INTO items (name, description, price, picture_url, active)
-VALUES ('Lollipops', 'candy flavoured', 059, 'https://images.pexels.com/photos/40834/eat-sugar-calories-food-40834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', false);
-INSERT INTO items (name, description, price, picture_url, active)
-VALUES ('Icecream', 'Icy and creamy!', 159, 'https://images.pexels.com/photos/3631/summer-dessert-sweet-ice-cream.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', false);
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Soft drink', 'Coca-cola', 149, 'https://images.pexels.com/photos/2983100/pexels-photo-2983100.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Water Bottle', 'Finally calorie free water! Buy now lose weight!', 199, 'https://i5.walmartimages.ca/images/Large/187/973/6000200187973.jpg');
+
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Orange Juice', 'Gain weight back that you lost from drinking water.', 299, 'https://previews.123rf.com/images/photomaru/photomaru1011/photomaru101100054/8297195-orange-juice-and-slices-of-orange-isolated-on-white.jpg');
+
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Iced Tea', 'Tea with ice!', 99, 'https://images.pexels.com/photos/792613/pexels-photo-792613.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Corona', 'Everyone''s favourite virus - we mean, beer!', 299, 'https://images.pexels.com/photos/2286972/pexels-photo-2286972.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Mini Martini', 'Tequila, Triple Sec & fresh lime juice with a salted rim', 859, 'https://images.pexels.com/photos/3407782/pexels-photo-3407782.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Caesar', 'Vodka, clamato juice and veggie garnishes', 659, 'https://images.pexels.com/photos/2842876/pexels-photo-2842876.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Yum Yum Burger', 'Delicious hamburger served with fries', 199, 'https://images.pexels.com/photos/3219483/pexels-photo-3219483.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Fettuccini', 'Chicken breast, shrimp, tomatoes, beans and marinara sauce', 1099, 'https://images.pexels.com/photos/1527603/pexels-photo-1527603.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url)
+VALUES
+  ('Pepperoni Pizza', 'Topped with mushrooms, peppers, olives, tomatoes and pickles', 1099, 'https://images.pexels.com/photos/803290/pexels-photo-803290.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');
+INSERT INTO items
+  (name, description, price, picture_url, active)
+VALUES
+  ('Lollipops', 'candy flavoured', 059, 'https://images.pexels.com/photos/40834/eat-sugar-calories-food-40834.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', false);
+INSERT INTO items
+  (name, description, price, picture_url, active)
+VALUES
+  ('Icecream', 'Icy and creamy!', 159, 'https://images.pexels.com/photos/3631/summer-dessert-sweet-ice-cream.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260', false);
