b757e90a33731f74104f45cdf955c1ef9f386062
Artur Iatsko <arturyatsko@gmail.com>
2020-03-10 22:08:27 +0000

    AJAX reload updated

--- public/scripts/orders_socket.js
+++ public/scripts/orders_socket.js
@@ -1,13 +1,12 @@
 $(() => {
   let socket = io();
-  socket.on("orderStatusChanged", function(data) {
+  socket.on("orderStatusChanged", function (data) {
     if (data.status === "pending") {
-      $.ajax({
-        type: "GET",
-        url: "/orders"
-      }).then(() => {
-        $(body).load("#body");
-      });
+      $.ajax('/orders', { method: 'GET' })
+        .then(function (data) {
+
+          $(document.body).replaceWith(data);
+        });
     }
   });
 });
