b1744538139c9ee78dab62bc2b8791cb7eca1c13
Artur Iatsko <arturyatsko@gmail.com>
2020-03-10 21:25:51 +0000

    Seeds updated

--- db/seeds/03_orders.sql
+++ db/seeds/03_orders.sql
@@ -1,18 +1,26 @@
 -- orders table seeds here
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
-VALUES(1, '2018-02-12T08:08:40.000Z', 'pending', 4094, '20 mins');
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
-VALUES(1, '2019-09-07T08:22:00.000Z', 'accepted', 2724, '5 mins');
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
+VALUES(1, '2018-02-12T08:08:40.000Z', 'pending', 4094, 20);
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
+VALUES(1, '2019-09-07T08:22:00.000Z', 'accepted', 2724, 5);
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
 VALUES(1, '2019-11-25T05:24:47.000Z', 'cancelled', 1099, NULL);
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
-VALUES(1, '2020-01-18T09:42:50.000Z', 'completed', 4650, '35 mins');
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
+VALUES(1, '2020-01-18T09:42:50.000Z', 'completed', 4650, 35);
 
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
-VALUES(1, '2018-12-12T08:08:40.000Z', 'pending', 2253, '15 mins');
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
-VALUES(1, '2019-05-07T08:22:00.000Z', 'accepted', 4960, '60 mins');
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
+VALUES(1, '2018-12-12T08:08:40.000Z', 'pending', 2253, 15);
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
+VALUES(1, '2019-05-07T08:22:00.000Z', 'accepted', 4960, 60);
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
 VALUES(1, '2019-02-25T05:24:47.000Z', 'cancelled', 4396, NULL);
-INSERT INTO orders (user_id, created_at, status, total_price, wait_time)
-VALUES(1, '2020-10-18T09:42:50.000Z', 'completed', 1794, '15 mins');
+INSERT INTO orders
+  (user_id, created_at, status, total_price, wait_time)
+VALUES(1, '2020-10-18T09:42:50.000Z', 'completed', 1794, 15);
