ae4cd19f0d6c010b808ae22436604ee397762875
Artur Iatsko <arturyatsko@gmail.com>
2020-03-10 21:40:26 +0000

    Added AJAX load on new order

--- public/scripts/orders_socket.js
+++ public/scripts/orders_socket.js
@@ -2,12 +2,21 @@ $(() => {
   let socket = io();
   socket.on("orderStatusChanged", function (data) {
     if (data.status === "pending") {
-      $.ajax({
-        type: "GET",
-        url: "/orders"
-      }).then(() => {
-        $("#body").load("#body");
-      });
+      // $.ajax({
+      //   type: "GET",
+      //   url: "/orders"
+      // }).then(() => {
+      //   $("#body").load("#body");
+      // });
+
+      $.ajax('/orders', { method: 'GET' })
+        .then(function (data) {
+          // let item = data[data.length - 1];
+          // $(document.body).remove();
+          $(document.body).replaceWith(data);
+        });
+
+
     }
   });
 });
