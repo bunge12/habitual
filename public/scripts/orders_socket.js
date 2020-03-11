3a8c4cf5cac693dfb887af9f0492dabe405a8c09
Zhao Wang <wangz2266@gmail.com>
2020-03-11 15:46:27 -0700

    modify socket

--- public/scripts/orders_socket.js
+++ public/scripts/orders_socket.js
@@ -4,6 +4,9 @@ $(() => {
     <source src="audio/alert2.mp3" type="audio/mpeg">
     </audio>
   `);
+  $(document).on("click", function() {
+    document.title = "Habitual App";
+  });
   let socket = io();
   socket.on("orderStatusChanged", function(data) {
     if (data.status === "pending") {
