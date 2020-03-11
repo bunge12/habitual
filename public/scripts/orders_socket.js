16c2dfe167efeba50d93c52788f84b8a199be94c
Artur Iatsko <arturyatsko@gmail.com>
2020-03-11 22:27:58 +0000

    Notifications done

--- public/scripts/orders_socket.js
+++ public/scripts/orders_socket.js
@@ -1,4 +1,9 @@
 $(() => {
+  $("div#body").append(`
+    <audio id="orderComingSound">
+    <source src="audio/alert2.mp3" type="audio/mpeg">
+    </audio>
+  `);
   let socket = io();
   socket.on("orderStatusChanged", function (data) {
     if (data.status === "pending") {
@@ -61,11 +66,23 @@ $(() => {
             </form>
           </div>
           `;
-
+        $toast = `<div class="toast" data-delay="7000" style="position: fixed;
+        bottom: 1rem;
+        right: 1rem;
+        z-index: 999;
+        width: 40%;">
+        <div class="toast-header">
+          <strong class="mr-auto text-primary">New Order Alert</strong>
+          <small class="text-muted">Just now!</small>
+          <button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button>
+        </div>
+        <div class="toast-body">
+          Order #${data.orderid} has just been submitted. Please check the pending orders tab in order to accept or cancel the order.
+        </div>
+      </div>`;
         $(".pen_ord").prepend($order);
-        $("div#body").append(`<audio id="orderComingSound">
-         <source src="audio/alert2.mp3" type="audio/mpeg">
-      </audio>`);
+        $(body).prepend($toast);
+        $('.toast').toast('show');
         $("div#body audio#orderComingSound")[0].play();
       });
     }
