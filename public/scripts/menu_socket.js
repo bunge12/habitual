e5300842dfc17dfa16a027b3f56a0fafa3c1e178
Zhao Wang <wangz2266@gmail.com>
2020-03-11 11:12:26 -0700

    add timer

--- public/scripts/menu_socket.js
+++ public/scripts/menu_socket.js
@@ -2,7 +2,33 @@ $(() => {
   let socket = io();
   socket.on("orderStatusChanged", function(data) {
     if (data.status === "accepted") {
-      $("#cart_header").html(`Ready in ${data.waitTime} minutes!`);
+      const makeTimer = function(completeTime) {
+        let timeLeft = (completeTime - Date.now()) / 1000;
+        let minutes = Math.floor(timeLeft / 60);
+        let seconds = Math.floor(timeLeft - minutes * 60);
+
+        if (minutes < "10") {
+          minutes = "0" + minutes;
+        }
+        if (seconds < "10") {
+          seconds = "0" + seconds;
+        }
+        let str = `${minutes} min ${seconds} sec.`;
+
+        $("#cart_header").html(`Ready in ${str}`);
+
+        if (timeLeft < 0) {
+          clearInterval(intervalId);
+          $("#cart_header").html("Checkout your order!");
+        }
+      };
+
+      const completeTime = Date.now() + data.waitTime * 60 * 1000;
+
+      const intervalId = setInterval(function() {
+        makeTimer(completeTime);
+      }, 1000);
+
       $("#submit_order")
         .html("Order Accepted!")
         .removeClass("btn btn-warning")
