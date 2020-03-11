45b4d7330f2ae63b7e026893a2e4c28e2b2a9b4e
Zhao Wang <wangz2266@gmail.com>
2020-03-10 20:13:35 -0700

    modify socket

--- public/scripts/menu_socket.js
+++ public/scripts/menu_socket.js
@@ -5,13 +5,19 @@ $(() => {
       $("#cart_header").html(`Ready in ${data.waitTime} min`);
       $("#submit_order")
         .html("Order Accepted!")
-        .addClass("btn btn-success text-white");
+        .addClass("btn btn-info text-white");
     }
     if (data.status === "cancelled") {
       $("#cart_header").html("Your order is cancelled.");
       $("#submit_order")
         .html("Order Cancelled.")
         .addClass("btn btn-danger text-white");
+    }
+    if (data.status === "completed") {
+      $("#cart_header").html("Your order is ready!");
+      $("#submit_order")
+        .html("Order Completed.")
+        .addClass("btn btn-success text-white");
     }
   });
 });
