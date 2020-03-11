7b50d3ba7eae079644b4f9bff99f9a9f24b1c2fd
Artur Iatsko <arturyatsko@gmail.com>
2020-03-11 20:42:54 +0000

    Sockets & Layout finalized

--- public/scripts/orders_socket.js
+++ public/scripts/orders_socket.js
@@ -20,50 +20,48 @@ $(() => {
           $order += `</ul>
           </div>
           <div class="card-footer pending">
-            <span class="float-right">
-              <div class="input-group pending-buttons">
-                <form action="/orders/${data.orderid}" method="POST" class="cancel">
-                  <input type="hidden" name="_method" value="PUT">
-                  <input type="hidden" name="status" value="cancelled">
-                  <input type="hidden" name="waitTime" value="null">
-                  <button id="c${data.orderid}"
-                    class="btn-sm btn-outline-danger float-right mr-2 cancel_button" type="submit">
-                    Cancel
-                  </button>
-                </form>
-
-                <div class="input-group-append">
-                  <form action="/orders/${data.orderid}" method="POST" class="accept">
-                    <input type="hidden" name="_method" value="PUT">
-                    <input type="hidden" name="status" value="accepted">
-                    <button id="a${data.orderid}" class="btn btn-sm btn-outline-success" type="button"
-                      data-toggle="collapse" data-target="#collapseExample${data.orderid}" aria-expanded="false"
+                <span class="float-right">
+                  <div class="input-group pending-buttons">
+                    <form action="/orders/${data.orderid}" method="POST" class="cancel">
+                      <input type="hidden" name="_method" value="PUT">
+                      <input type="hidden" name="status" value="cancelled">
+                      <input type="hidden" name="waitTime" value="null">
+                      <button id="c${data.orderid}"
+                        class="btn-sm btn-outline-danger float-right mr-2 cancel_button" type="submit">
+                        Cancel
+                      </button>
+                    </form>
+                    <button class="btn btn-sm btn-outline-success" type="button" data-toggle="collapse"
+                      data-target="#collapseExample${data.orderid}" aria-expanded="false"
                       aria-controls="collapseExample">
                       Accept
                     </button>
-                </div>
+
+                  </div>
+                </span>
               </div>
-            </span>
-          </div>
-          <div class="collapse" id="collapseExample${data.orderid}">
-            <div class="card card-body">
-              <div class="form-row">
-                <div class="col-4 text-right align-bottom mt-1">
-                  Enter wait time
-                </div>
-                <div class="col-4">
-                  <input type="text" class="form-control form-control-sm" placeholder="(minutes)" name="waitTime"
-                    maxlength="2" size="2" required />
-                </div>
-                <div class="col-4">
-                  <button class="btn btn-sm btn-success" type="submit" name="accept">Accept
-                    Order</button>
-                </div>
+              <div class="collapse" id="collapseExample${data.orderid}">
+                <form action="/orders/${data.orderid}" method="POST" class="accept">
+                  <input type="hidden" name="_method" value="PUT">
+                  <input type="hidden" name="status" value="accepted">
+                  <div class="card card-body">
+                    <div class="form-row">
+                      <div class="col-4 text-right align-bottom mt-1">
+                        Enter wait time
+                      </div>
+                      <div class="col-4">
+                        <input type="text" class="form-control form-control-sm" placeholder="(minutes)" name="waitTime"
+                          maxlength="2" size="2" required />
+                      </div>
+                      <div class="col-4">
+                        <button id="a${data.orderid}" class="btn btn-sm btn-success" type="submit">Accept
+                          Order</button>
+                      </div>
+                    </div>
+                  </div>
               </div>
-            </div>
-          </div>
-          </form>
-        </div>`;
+              </form>
+            </div>`;
           // let newHTML = concat($order, $order2);
           $(".pen_ord").prepend($order);
         });
